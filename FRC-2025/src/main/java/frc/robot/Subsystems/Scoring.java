20909cc89f0e052af89fdb72d2a7ad2e8205331e
thetiger301 <j.alexander.traeger@gmail.com>
2025-01-18 22:50:25 -0500

    Declared all Methods in Scoring

--- FRC-2025/src/main/java/frc/robot/Subsystems/Scoring.java
+++ FRC-2025/src/main/java/frc/robot/Subsystems/Scoring.java
@@ -8,31 +8,43 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Scoring extends SubsystemBase {
-    public static SparkMax scoringMotor1;
-    public static SparkMax scoringMotor2;
-    public static SparkMax scoringMotor3;
+    public static SparkMax angle;
+    public static SparkMax elevator;
     public static SparkMax roller;
     public static DigitalInput sensor;
     private static Scoring m_Instance = null;
 
-    public Scoring(int scoringMotor1ID, int scoringMotor2ID, int scoringMotor3ID, int rollerID, int sensorID) {
-        scoringMotor1 = new SparkMax(scoringMotor1ID, MotorType.kBrushless);
-        scoringMotor2 = new SparkMax(scoringMotor2ID, MotorType.kBrushless);
-        scoringMotor3 = new SparkMax(scoringMotor3ID, MotorType.kBrushless);
+    public Scoring(int angleID, int elevatorID, int rollerID, int sensorID) {
+        angle = new SparkMax(angleID, MotorType.kBrushless);
+        elevator = new SparkMax(elevatorID, MotorType.kBrushless);
+        roller = new SparkMax(rollerID, MotorType.kBrushless);
         sensor = new DigitalInput(sensorID);
     }
-
+    // Checks if limit switch is clear
     public boolean isScoringMecCLear(){
         return sensor.get();
     }
-
+    // Turns on Roller on scoring mecanism
     public void setRollerSpeed(double speed) {
         roller.set(speed);
     }
- 
+    // Set elevator and anlge to levels with encoder ticks
+    public void elevatorLevel0(){
+    }
+    
+    public void elevatorLevel2() {
+
+    }
+
+    public void elevatorLevel3() {
+    }
+
+    public void elevatorLevel4() {
+
+    }
     public static Scoring getInstance() {
         if(m_Instance == null)
-            m_Instance = new Scoring(Constants.Scoring.scoringMotor1ID, Constants.Scoring.scoringMotor2ID, Constants.Scoring.scoringMotor3ID, Constants.Scoring.rollerID, Constants.Scoring.sensorID);
+            m_Instance = new Scoring(Constants.Scoring.angleID, Constants.Scoring.elevatorID, Constants.Scoring.rollerID, Constants.Scoring.sensorID);
             return m_Instance;
         }
     }
