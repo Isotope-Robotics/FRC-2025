899fb46011a8e7b8b989481f51127aa5aaa7f623
thetiger301 <j.alexander.traeger@gmail.com>
2025-01-19 09:44:50 -0500

    Started work on adding encoder capability to Scoring Subsystem

--- FRC-2025/src/main/java/frc/robot/Subsystems/Scoring.java
+++ FRC-2025/src/main/java/frc/robot/Subsystems/Scoring.java
@@ -3,22 +3,29 @@ package frc.robot.Subsystems;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+
 
 public class Scoring extends SubsystemBase {
     public static SparkMax angle;
     public static SparkMax elevator;
     public static SparkMax roller;
     public static DigitalInput sensor;
+    public static DutyCycleEncoder dutyCycleEncoder;
+    public static final double m_fullRange = 1.3;
+    public static final double m_expectedZero = 0;
+
     private static Scoring m_Instance = null;
 
-    public Scoring(int angleID, int elevatorID, int rollerID, int sensorID) {
+    public Scoring(int angleID, int elevatorID, int rollerID, int sensorID, int dutyCycleEncoderID) {
         angle = new SparkMax(angleID, MotorType.kBrushless);
         elevator = new SparkMax(elevatorID, MotorType.kBrushless);
         roller = new SparkMax(rollerID, MotorType.kBrushless);
         sensor = new DigitalInput(sensorID);
+        dutyCycleEncoder = new DutyCycleEncoder(dutyCycleEncoderID, m_fullRange, m_expectedZero);
     }
     // Checks if limit switch is clear
     public boolean isScoringMecCLear(){
