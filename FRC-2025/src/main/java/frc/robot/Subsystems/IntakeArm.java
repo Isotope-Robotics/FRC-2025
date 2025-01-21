feb5d648fc06d6ee2dca7630c5cf13ca29949bb1
ChillMarto <113388084+ChillMarto@users.noreply.github.com>
2025-01-20 19:54:30 -0500

    Updated intakearm.java

---
+++ FRC-2025/src/main/java/frc/robot/Subsystems/IntakeArm.java
@@ -0,0 +1,36 @@
+package frc.robot.Subsystems;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class IntakeArm extends SubsystemBase  {
+    private static SparkMax armMotor;
+    private static IntakeArm m_Instance = null;
+    DigitalInput outsideLimitSwitch = new DigitalInput(0);
+    DigitalInput insideLimitSwitch = new DigitalInput(1);
+    public IntakeArm(int intakeArmMotorID) {
+        armMotor = new SparkMax(intakeArmMotorID, MotorType.kBrushless);
+    }
+    public boolean isInsideSwitchPressed(){
+        return insideLimitSwitch.get();
+    }
+    public boolean isOutsideSwitchPressed(){
+        return outsideLimitSwitch.get();
+    }
+    public void moveArmOut(){
+        armMotor.set(1);
+    }
+    public void moveArmIn(){
+        armMotor.set(1);
+    }
+    public void stopArm(){
+        armMotor.set(0);
+    }
+    public static IntakeArm getInstance() {
+        if (m_Instance == null)
+            m_Instance = new IntakeArm(Constants.IntakeArm.intakeArmMotorID);
+        return m_Instance;
+    }
+}
