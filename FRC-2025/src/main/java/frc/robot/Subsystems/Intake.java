a91e1fe6545671a4dfe5937f8c51b03ec0a8298d
scientistofeverything <noah.e.haskins@gmail.com>
2025-01-20 19:15:05 -0500

    Accomodate for updated intake design

--- FRC-2025/src/main/java/frc/robot/Subsystems/Intake.java
+++ FRC-2025/src/main/java/frc/robot/Subsystems/Intake.java
@@ -6,9 +6,7 @@ import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.RelativeEncoder;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -18,45 +16,29 @@ public class Intake extends SubsystemBase {
     // this spins in an arbitrary direction to intake a coral
     public static SparkMax intakeMotor;
 
-    // this runs between two set positions to pivot the intake
-    public static SparkMax pivotMotor;
-
-    public static final PIDController pivotPID = new PIDController(Constants.Intake.kP, Constants.Intake.kI, Constants.Intake.kD);
-
-    // photoelectric sensor to detect coral
+    // photoelectric sensor to detect coral having been taken in
     public static DigitalInput coralDetector;
-
-    public static RelativeEncoder pivotEncoder;
 
     private static Intake m_Instance = null;
 
-    public Intake (int intakeMotorID, int pivotMotorID) {
+    private static boolean coralStatus = false;
+
+    public Intake (int intakeMotorID) {
         SparkMaxConfig intakeConfig = new SparkMaxConfig();
-        SparkMaxConfig pivotConfig = new SparkMaxConfig();
         intakeConfig.idleMode(IdleMode.kBrake);
-        pivotConfig.idleMode(IdleMode.kBrake);
         intakeMotor = new SparkMax(intakeMotorID, MotorType.kBrushless);
-        pivotMotor = new SparkMax(pivotMotorID, MotorType.kBrushless);
         intakeMotor.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-        pivotMotor.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-        pivotEncoder = pivotMotor.getEncoder();
 
         coralDetector = new DigitalInput(0); // this port number is probably wrong!!!
     }
-
-    public void toRobot () {
-        // move pivot motor to floor positon
-        pivotMotor.set(pivotPID.calculate(pivotEncoder.getPosition(), 0.0));
-    }
-
-    public void toFloor () {
-        // move pivot motor to initial position
-        pivotMotor.set(pivotPID.calculate(pivotEncoder.getPosition(), 14.0));
-    }
-
+    
     public void runIn () {
         // power intake motor
         intakeMotor.set(1.0); // this speed might be wrong!!!
+        if (coralDetector1.get())
+            coralStatus = true;
+        else
+            coralStatus = false;
     }
 
     public void runOut () {
@@ -68,12 +50,8 @@ public class Intake extends SubsystemBase {
         intakeMotor.set(0.0);
     }
 
-    public void pivotStop () {
-        pivotMotor.set(0.0);
-    }
-
-    public boolean hasCoral () {
-        return !coralDetector.get();
+    public int getCoralStatus () {
+        return coralStatus;
     }
 
     public static Intake getInstance() {
