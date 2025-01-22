1751cbe105ea02a4f2325197ca2c7b7fdc619d0a
Ethen <ebot14rocks@gmail.com>
2025-01-21 22:49:12 -0600

    fixed some typos

--- FRC-2025/src/main/java/frc/robot/Subsystems/Intake.java
+++ FRC-2025/src/main/java/frc/robot/Subsystems/Intake.java
@@ -35,7 +35,7 @@ public class Intake extends SubsystemBase {
     public void runIn () {
         // power intake motor
         intakeMotor.set(1.0); // this speed might be wrong!!!
-        if (coralDetector1.get())
+        if (coralDetector.get())
             coralStatus = true;
         else
             coralStatus = false;
@@ -50,13 +50,13 @@ public class Intake extends SubsystemBase {
         intakeMotor.set(0.0);
     }
 
-    public int getCoralStatus () {
+    public boolean getCoralStatus () {
         return coralStatus;
     }
 
     public static Intake getInstance() {
         if (m_Instance == null)
-            m_Instance = new Intake(Constants.Intake.intakeMotorID, Constants.Intake.pivotMotorID);
+            m_Instance = new Intake(Constants.Intake.intakeMotorID);
         return m_Instance;
     }
 
