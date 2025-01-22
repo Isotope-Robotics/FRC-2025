9497f988e3c1045bb54f2149d7fe51f64b0717db
dubz7972 <wesley.traeger@gmail.com>
2025-01-22 18:50:20 -0500

    made tons of changes to robot.java

--- FRC-2025/src/main/java/frc/robot/Subsystems/Intake.java
+++ FRC-2025/src/main/java/frc/robot/Subsystems/Intake.java
@@ -50,7 +50,7 @@ public class Intake extends SubsystemBase {
         intakeMotor.set(0.0);
     }
 
-    public int getCoralStatus () {
+    public boolean getCoralStatus () {
         return coralStatus;
     }
 
