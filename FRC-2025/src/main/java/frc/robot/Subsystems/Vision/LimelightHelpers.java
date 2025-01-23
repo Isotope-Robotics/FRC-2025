47078fb7ffbdef496a605ca6de8c222f44c5ac34
thetiger301 <j.alexander.traeger@gmail.com>
2025-01-22 19:21:15 -0500

    Moved Vision to subsystems

--- FRC-2025/src/main/java/frc/robot/Subsystems/Vision/LimelightHelpers.java
+++ FRC-2025/src/main/java/frc/robot/Subsystems/Vision/LimelightHelpers.java
@@ -1,14 +1,14 @@
 //LimelightHelpers v1.10 (REQUIRES LLOS 2024.9.1 OR LATER)
 
-package frc.robot.Vision;
+package frc.robot.Subsystems.Vision;
 
 import edu.wpi.first.networktables.DoubleArrayEntry;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.TimestampedDoubleArray;
-import frc.robot.Vision.LimelightHelpers.LimelightResults;
-import frc.robot.Vision.LimelightHelpers.PoseEstimate;
+import frc.robot.Subsystems.Vision.LimelightHelpers.LimelightResults;
+import frc.robot.Subsystems.Vision.LimelightHelpers.PoseEstimate;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
